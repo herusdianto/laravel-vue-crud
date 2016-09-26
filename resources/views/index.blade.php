dfc921e1894465688cbeaa77d3eec7d6b18e7534
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-26 16:46:50 +0700

    created index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel & VueJS CRUD</title>
+
+    <link rel="stylesheet" href="{{ elixir('css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+</head>
+<body>
+    <div class="container mrg-top-2" id="app">
+        <router-view></router-view>
+    </div>
+
+    <script>
+        window.Laravel = { csrfToken: '{{ csrf_token() }}' };
+    </script>
+    <script src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
