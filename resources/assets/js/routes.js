6ab9286196bc3543f5ddaa4ed2d3eaa5ba3b20e7
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-26 22:56:47 +0700

    added show student route

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,10 +1,16 @@
 const router = new VueRouter();
 
 const index = require('./components/Index.vue');
+const show = require('./components/Show.vue');
 
 router.map({
     '/': {
+        name: 'index',
         component: index
+    },
+    '/show/:studentId': {
+        name: 'show',
+        component: show
     }
 });
 
