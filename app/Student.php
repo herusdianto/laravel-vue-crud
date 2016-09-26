151e067c9ab2b3a8b3f60fda88a04cf2152f4c78
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-26 16:44:25 +0700

    created student model

---
+++ app/Student.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'avatar', 'name', 'email', 'birth_date',
+    ];
+
+}
