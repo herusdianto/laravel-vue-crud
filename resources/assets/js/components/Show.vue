3b75da7daa921ce2ba0f8fb749e8fb84d4c64de2
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-26 22:56:58 +0700

    added show component

---
+++ resources/assets/js/components/Show.vue
@@ -0,0 +1,53 @@
+<template v-cloak>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="" alt="" class="img-thumbnail img-responsive">
+                </div>
+
+                <div class="col-md-6">
+                    <p>
+                        <label>Name:</label>
+                        {{ student.name }}
+                    </p>
+
+                    <p>
+                        <label>Email:</label>
+                        {{ student.email }}
+                    </p>
+
+                    <p>
+                        <label>Birth Date:</label>
+                        {{ student.birth_date }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center">
+        <button class="btn btn-default btn-sm" v-link="{ path: '/' }">
+            &laquo; Back
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        created() {
+            this.$http.get('/api/students/' + this.$route.params.studentId)
+                .then(response => {
+                    this.student = response.data
+                })
+                .catch(response => {
+                    console.error('Error: ' + response.statusText);
+                });
+        },
+        data() {
+            return {
+                student: {}
+            };
+        }
+    }
+</script>
