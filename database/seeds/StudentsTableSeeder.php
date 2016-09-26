239a57ebc8d2bfa7ba0f985f759dd9f63cab07c0
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-26 16:30:51 +0700

    created students table seeder

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class StudentsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(StudentsTableSeeder::class);
+        factory(App\Student::class, 100)->create();
     }
 }
