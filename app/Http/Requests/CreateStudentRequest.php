3e9c312f00b6f9fd243fa3c3ef9a4cac2df46c1c
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-28 16:21:08 +0700

    created CreateStudentRequest

---
+++ app/Http/Requests/CreateStudentRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'avatar' => 'image',
+            'name' => 'required',
+            'email' => 'required|email|unique:students,email',
+            'birth_date' => 'required|date',
+        ];
+    }
+}
