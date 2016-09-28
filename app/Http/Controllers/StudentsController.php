fc404b6a85b9c07ec0fe5183285e5e5c672c4481
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-09-28 16:41:15 +0700

    remove unused method

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -40,16 +40,6 @@ class StudentsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param CreateStudentRequest $request
@@ -86,17 +76,6 @@ class StudentsController extends Controller
     public function show($id)
     {
         return Student::findOrFail($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
